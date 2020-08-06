3738996b85c0b3fd1333f93f32f31355175832ee
Ashu <shopashu@gmail.com>
2020-08-06 21:54:51 +0530

    Updates

---
+++ precache-manifest.2b598bf2306bf231b8081a40d702b196.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "743f24fea446f5bfc08586bde855cd86",
+    "url": "/my-resume/index.html"
+  },
+  {
+    "revision": "530b96743cc5f88281c7",
+    "url": "/my-resume/static/css/3.48a93ee6.chunk.css"
+  },
+  {
+    "revision": "4aec19b2175d6fdbc3b1",
+    "url": "/my-resume/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "c0dc5e7ad2d804a5e1bf",
+    "url": "/my-resume/static/css/main.90376c79.chunk.css"
+  },
+  {
+    "revision": "963e370c131f1f1306a6",
+    "url": "/my-resume/static/js/2.1520498a.chunk.js"
+  },
+  {
+    "revision": "530b96743cc5f88281c7",
+    "url": "/my-resume/static/js/3.d9d39de4.chunk.js"
+  },
+  {
+    "revision": "4aec19b2175d6fdbc3b1",
+    "url": "/my-resume/static/js/4.526ffc1e.chunk.js"
+  },
+  {
+    "revision": "c0dc5e7ad2d804a5e1bf",
+    "url": "/my-resume/static/js/main.04c377fa.chunk.js"
+  },
+  {
+    "revision": "24c848815d409606acc1",
+    "url": "/my-resume/static/js/runtime-main.c8b4216e.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/my-resume/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/my-resume/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/my-resume/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/my-resume/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/my-resume/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/my-resume/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/my-resume/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-resume/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/my-resume/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/my-resume/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/my-resume/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/my-resume/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/my-resume/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/my-resume/static/media/talksCardBack.de3da806.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.42a735cf375c8aaacae0e0557c2e742c.js"
+  "/my-resume/precache-manifest.2b598bf2306bf231b8081a40d702b196.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-resume/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
